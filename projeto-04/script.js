91d76bf4be31da3856f14f7402a03095503085b1
Igor Gazio <gaumgazineo@hotmail.com>
2025-01-22 14:19:52 -0300

    resolvendo bug do projeto-04

--- projeto-04/script.js
+++ projeto-04/script.js
@@ -1,8 +1,14 @@
 var btn = document.querySelector("button");
+var paragraphEl = document.querySelector("p");
 var inputEl = document.querySelector("input");
 var bestMovie = "Internet O Filme";
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
-    var paragraphEl = document.querySelector("p");
-    paragraphEl.innerText = paragraphEl.innerText.replace(bestMovie, "".concat(inputEl.value));
-    bestMovie = inputEl.value;
+btn.addEventListener("click", function () {
+    if (inputEl.value) {
+        paragraphEl.innerText = paragraphEl.innerText.replace(bestMovie, "".concat(inputEl.value));
+        bestMovie = inputEl.value;
+    }
+    else {
+        paragraphEl.innerText = 'O melhor filme do mundo é "Internet O Filme"';
+        bestMovie = "Internet O Filme";
+    }
 });
--- projeto-04/script.ts
+++ projeto-04/script.ts
@@ -1,13 +1,18 @@
 const btn = document.querySelector("button") as HTMLButtonElement;
+const paragraphEl = document.querySelector("p") as HTMLParagraphElement;
 const inputEl = document.querySelector("input") as HTMLInputElement;
 
 let bestMovie = "Internet O Filme";
 
-btn?.addEventListener("click", () => {
-  const paragraphEl = document.querySelector("p") as HTMLParagraphElement;
-  paragraphEl.innerText = paragraphEl.innerText.replace(
-    bestMovie,
-    `${inputEl.value}`
-  );
-  bestMovie = inputEl.value;
+btn.addEventListener("click", () => {
+  if (inputEl.value) {
+    paragraphEl.innerText = paragraphEl.innerText.replace(
+      bestMovie,
+      `${inputEl.value}`
+    );
+    bestMovie = inputEl.value;
+  } else {
+    paragraphEl.innerText = 'O melhor filme do mundo é "Internet O Filme"';
+    bestMovie = "Internet O Filme";
+  }
 });
